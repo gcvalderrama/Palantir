0e8151a9f96a2be701b8ae3b6d955f7e8b3c17a2
valderramag <gcvalderrama@hotmail.com>
2016-06-23 00:07:17 -0500

    m

--- 00/Code/Program.py
+++ 00/Code/Program.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-if __name__ == "__main__":
+def obtain_cap():
     corpus_anotator_00 = "./../anotador_00"
     corpus_anotator_01 = "./../anotador_01"
 
@@ -30,6 +30,29 @@ if __name__ == "__main__":
     print("01 anotator attack {}".format(len(corpus_anotator_01_attack)))
     print("01 anotator nonattack {}".format(len(corpus_anotator_01_nonattack)))
 
+    anotator_attack_diff = 0
     for file in corpus_anotator_00_attack:
         if file not in corpus_anotator_01_attack:
-            print(file)
+            anotator_attack_diff += 1
+    print("00 anotator attack diff {}".format(anotator_attack_diff))
+
+    anotator01_attack_diff = 0
+    for file in corpus_anotator_01_attack:
+        if file not in corpus_anotator_00_attack:
+            anotator01_attack_diff += 1
+    print("01 anotator attack diff {}".format(anotator01_attack_diff))
+
+    anotator_nonattack_diff = 0
+    for file in corpus_anotator_00_nonattack:
+        if file not in corpus_anotator_01_nonattack:
+            anotator_nonattack_diff += 1
+    print("00 anotator nonattack diff {}".format(anotator_nonattack_diff))
+
+    anotator01_nonattack_diff = 0
+    for file in corpus_anotator_01_nonattack:
+        if file not in corpus_anotator_00_nonattack:
+            anotator01_nonattack_diff += 1
+    print("01 anotator attack diff {}".format(anotator01_nonattack_diff))
+
+if __name__ == "__main__":
+    print('test')
