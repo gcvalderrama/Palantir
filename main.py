086f14f0b1974094d18284ce364bb6171231600e
Gregory Valderrama <gcvalderrama@hotmail.com>
2016-06-23 01:12:11 -0500

    testing with stemm

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
 from worker import Crawler, Helper, Trainer
 
-
 # # extract news from url and save them
 # webCrawler = Crawler()
 # webCrawler.extract_web_news('news', 'http://elcomercio.pe/feed/lima/policiales.xml', 'attack')
@@ -22,11 +21,19 @@ from worker import Crawler, Helper, Trainer
 
 # remove stop words from news and prepare text for training
 news_trainer = Trainer()
-# news_trainer.tokenize_files('news/clean', '00/attack')
+news_trainer.tokenize_files('00/anotador_01', '01/clean_news')
+news_trainer.stemming_files('00/anotador_01', '01/stemm_news')
 
-# begin training
-news_trainer.train_classifier('classification/corpusnews', 'train', 'devtest')
+# begin training with normal words
+news_trainer.train_classifier('01', 'clean_news')
 news_trainer.compare_classifiers_accuracy()
 type = news_trainer.classify_document(
     'classification/corpusnews/devtest/nonattack--india-espera-compartir-experiencias-gobierno-ppk-noticia-1910298.txt')
-print(type)
+
+print('************************************************')
+# training with stemmed words
+news_trainer.train_classifier('01', 'stemm_news')
+news_trainer.compare_classifiers_accuracy()
+type = news_trainer.classify_document(
+    'classification/corpusnews/devtest/nonattack--india-espera-compartir-experiencias-gobierno-ppk-noticia-1910298.txt')
+
--- worker/trainer.py
+++ worker/trainer.py
@@ -1,13 +1,17 @@
 """
 This script process all the clean news, to determine if they are into 'attack' or 'nonattack' category
 """
+import os
 import glob
+import math
 import random
 import pickle
 import unicodedata
 from nltk import NaiveBayesClassifier, FreqDist, classify
 from nltk.corpus import stopwords, PlaintextCorpusReader
 from nltk.classify.scikitlearn import SklearnClassifier
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.snowball import SnowballStemmer
 from nltk.tokenize import word_tokenize
 
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB
@@ -93,6 +97,44 @@ class Trainer:
         with open('naives_classifier.pickle', 'wb') as save_classifier:
             pickle.dump(classifier, save_classifier)
 
+    def train_classifier(self, root_folder, train_folder, files_extension='txt'):
+        """
+        Generates .pickle file with trained classifier and words universe
+        :param root_folder: Folder that contains train and devtest folders
+        :param train_folder: Folder inside root_folder that contains the news to train with
+        :param files_extension: File extension to search for
+        :return: void - empty
+        """
+        train_news_files = glob.glob(root_folder + "/" + train_folder + "/*." + files_extension)
+        corpus_news = PlaintextCorpusReader(root_folder, '.*\.' + files_extension)
+        words_docs = self.get_documents_words(train_news_files, corpus_news)
+
+        all_words = FreqDist(word.lower() for word in corpus_news.words())
+        word_features = list(all_words.keys())
+
+        with open('word_features.pickle', 'wb') as words_saver:
+            pickle.dump(word_features, words_saver)
+
+        features_set = [(self.find_features(news, word_features), category) for (news, category) in words_docs]
+        index = math.floor(len(features_set)/2)
+        training_set = features_set[:index]
+        testing_set = features_set[index:]
+
+        with open('training_set.pickle', 'wb') as training_writer:
+            pickle.dump(training_set, training_writer)
+        with open('devtesting_set.pickle', 'wb') as devtesting_writer:
+            pickle.dump(testing_set, devtesting_writer)
+
+        classifier = NaiveBayesClassifier.train(training_set)
+
+        accuracy = classify.accuracy(classifier, testing_set)
+
+        print('Naive Bayes accuracy percent: ', (accuracy * 100))
+        classifier.show_most_informative_features(20)
+        # saves classifier progress to a pickle file
+        with open('naives_classifier.pickle', 'wb') as save_classifier:
+            pickle.dump(classifier, save_classifier)
+
     def compare_classifiers_accuracy(self):
         with open('naives_classifier.pickle', 'rb') as read_classifier:
             naive_bayes_classifier = pickle.load(read_classifier)
@@ -161,22 +203,22 @@ class Trainer:
         result = naive_bayes_classifier.classify(text_feature)
         return result
 
-    def clean_tokenize(self, doc):
+    def clean_tokenize(self, input_text):
         """
         Clean document, removing accents, punctuation and symbols
-        :param doc: string to clean
+        :param text: string to clean
         :return: string cleaned without punctuation and stop words
         """
-        doc = doc.replace('\n', ' ').replace('\r', '').replace('”', '').replace('“', '')
-        nfkd_form = unicodedata.normalize('NFKD', doc)
-        unicode_doc = u"".join([c for c in nfkd_form if not unicodedata.combining(c)]).lower()
-        clean_doc = unicode_doc.translate(punctuation)
-        words = word_tokenize(clean_doc)
-        clean = []
+        text = input_text.replace('\n', ' ').replace('\r', '').replace('”', '').replace('“', '')
+        nfkd_form = unicodedata.normalize('NFKD', text)
+        unicode_text = u"".join([c for c in nfkd_form if not unicodedata.combining(c)]).lower()
+        clean_text = unicode_text.translate(punctuation)
+        words = word_tokenize(clean_text)
+        final_text = []
         for word in words:
             if word not in self.ALL_STOPWORDS:
-                clean.append(word)
-        return clean
+                final_text.append(word)
+        return final_text
 
     def tokenize_files(self, source_folder, destination_folder):
         """
@@ -185,16 +227,46 @@ class Trainer:
         :param destination_folder: Destination folder where news will be created
         :return: void - Generates all the destination files with clean text
         """
+        if not os.path.exists(destination_folder):
+            os.makedirs(destination_folder)
         news = glob.glob(source_folder + "/*.txt")
         for news_file in news:
-            file_name = news_file.split('/')[1]
+            file_name = news_file.split('/')[-1]
             with open(news_file, 'r') as original:
                 doc_text = original.read()
-            tokenize_cont = self.clean_tokenize(doc_text)
+            tokenize_content = self.clean_tokenize(doc_text)
             with open(destination_folder+"/"+file_name, 'w') as modified:
-                modified.write(' '.join(tokenize_cont))
+                modified.write(' '.join(tokenize_content))
 
+    def stemming_text(self, input_text):
+        """
+        Clean document, removing accents, punctuation and symbols and stemm words
+        :param text: string to clean
+        :return: string cleaned without punctuation and stop words
+        """
+        text = input_text.replace('\n', ' ').replace('\r', '').replace('”', '').replace('“', '')
+        nfkd_form = unicodedata.normalize('NFKD', text)
+        unicode_text = u"".join([c for c in nfkd_form if not unicodedata.combining(c)]).lower()
+        clean_text = unicode_text.translate(punctuation)
+        words = word_tokenize(clean_text)
+        stemmer = SnowballStemmer("spanish")
+        final_text = []
+        for word in words:
+            if word not in self.ALL_STOPWORDS:
+                final_text.append(stemmer.stem(word))
+        return final_text
 
+    def stemming_files(self, source_folder, destination_folder, extension='txt'):
+        if not os.path.exists(destination_folder):
+            os.makedirs(destination_folder)
+        file_list = glob.glob(source_folder + "/*." + extension)
+        for file in file_list:
+            file_name = file.split('/')[-1]
+            with open(file, 'r') as original:
+                original_text = original.read()
+            stemmed_content = self.stemming_text(original_text)
+            with open(destination_folder + "/" + file_name, 'w') as modified:
+                modified.write(' '.join(stemmed_content))
 
 # remove_first_line('cleanNews', 'txt')
 
